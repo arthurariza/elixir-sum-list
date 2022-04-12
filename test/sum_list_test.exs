a2e89dbab37ed9ec526f1aa70e1946b5b2f29e28
arthurariza <tucaariza@gmail.com>
2022-04-11 21:22:02 -0300

    test: SumList call/1

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,8 +1,29 @@
 defmodule SumListTest do
-  use ExUnit.Case
-  doctest SumList
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "when given an empty list returns 0 (accumulator)" do
+      empty_list = []
+
+      response = SumList.call(empty_list)
+
+      assert response == 0
+    end
+
+    test "when given a valid list, returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      assert response == 6
+    end
+
+    test "when given a list with negative values" do
+      negative_list = [5, -10, 10]
+
+      response = SumList.call(negative_list)
+
+      assert response == 5
+    end
   end
 end
